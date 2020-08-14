90b4d871162b47bca5965a009f59ff44f2309e35
fitchad <adamcfitch@gmail.com>
2020-08-14 16:39:28 -0400

    Add file

---
+++ UtilityScripts/join_data_by_key.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 13:02:12 2020
+
+This program will take an input file of columns, scan through
+the first column and add the unique values as keys in a dictionary.
+Then it will go through each column of data and add the unique values
+in each column as values to the key from the first column ignoring
+any duplicate values. It will then save the resulting dictionary into
+a .csv file. 
+
+
+
+To do:
+1. {DONE} Have the program parse part of a name to use as dictionary key
+2. Make this work for every column. User will set an ID column to merge
+on. Program will then take each column, create a list of metadata separated
+by semicolons, and separate each metadata column by commas.
+3. {DONE} How to handle missing data. inserts empty value. 
+4. Implement regex matching.
+
+
+
+
+Parsing is designed to parse an ID in the first column and pull out a field (same field in each),
+then use that field as a key. 
+If you don't use the parse option, then the program will assume the first column
+is the key value. 
+
+
+@author: acf
+"""
+
+from collections import defaultdict
+#import re
+import os
+import csv
+import argparse
+#import sys
+
+
+cwd=os.getcwd()
+
+
+
+
+def get_args():
+    '''This function parses and return arguments passed in'''
+    # Help Document Descriptions
+    parser = argparse.ArgumentParser(
+        description='')
+    # Add arguments
+    parser.add_argument(
+        '-l', '--fastqlist', type=str, help='location of .tsv fastq file list', required=True, default=None)
+    parser.add_argument('-f', '--field', type = int, help = 'field to use as key from filename, numbering starting from 1', required=False)
+    parser.add_argument('-d', '--delimiter', type=str, help='delimiter character', required=False)    
+    parser.add_argument('-D', '--Duplicated', help = 'if set, will keep duplicate values', required=False, default=False, action='store_true')
+    parser.add_argument(
+        '-o', '--outputfile', type=str, help='location file to write output. default is "concat_file.csv" in current dir', required=False, default=cwd+"/concat_file.csv")
+    # Array for all arguments passed to script
+    args = parser.parse_args()
+    
+    # Assign args to variables
+    fastqlist = args.fastqlist
+    field=args.field
+    delimiter=args.delimiter
+    Duplicated=args.Duplicated
+    outputfile=args.outputfile
+    # Return all variable values
+    return fastqlist, field, delimiter, Duplicated, outputfile
+
+
+# Match return values from get_arguments()
+# and assign to their respective variables
+fastqlist, field, delimiter, Duplicated, outputfile = get_args()
+
+
+parse=False
+if field:
+    
+    parse = True
+    #parse = parse - 1
+    parse_field = field-1
+
+
+## Variables ##
+
+fdictionary = defaultdict()
+
+
+
+## Functions ##
+
+def table_dict_to_csv(output, dictionary):
+    with open(output, 'wb') as f:  # Just use 'w' mode in 3.x
+        w = csv.writer(f, delimiter=",")
+        #w = csv.writer(sys.stderr) #for debugging, prints to stdout
+        #w.writerow(["SampleID", "R1]) #writes header row
+        for key, values in dictionary.iteritems():
+            templist = []
+            templist.append(key)
+            for val in values: # this approach removes brackets in output
+                templist.append(val)
+            w.writerow(templist)
+            
+
+### Main Program ###
+
+with open(fastqlist) as inputSheet:
+    for item in inputSheet:
+
+        if parse: #parse out key value from names in first column
+            row_items=item.strip("\n")
+            list_length = len(row_items) #number of columns
+            
+            row_items_split = row_items.split(",") #split row
+            sample_key = row_items_split[0] # get first item from split row
+            #print sample_key
+            sample_key_parse = sample_key.split(delimiter) # split first item by delimiter
+            #print sample_key_parse
+            if sample_key_parse[parse_field] not in fdictionary.keys():
+                fdictionary[sample_key_parse[parse_field]]=[]
+                if list_length > 1:
+                    for item in row_items_split[1:list_length]:
+                       clean_item=item.strip('\n')
+                       if Duplicated:
+                           fdictionary[sample_key_parse[parse_field]].append(clean_item)
+                       elif clean_item not in fdictionary[sample_key_parse[parse_field]]: #won't write duplicate value
+                            fdictionary[sample_key_parse[parse_field]].append(clean_item)
+            else:
+                 if list_length > 1:               
+                    for item in row_items_split[1:list_length]:
+                        clean_item=item.strip('\n')
+                        if Duplicated:
+                             fdictionary[sample_key_parse[parse_field]].append(clean_item)
+
+                        #print clean_item
+                        elif clean_item not in fdictionary[sample_key_parse[parse_field]]:
+                            fdictionary[sample_key_parse[parse_field]].append(clean_item)
+
+        
+     
+        
+        else: # no parsing of key from name
+            A=item.strip("\n")
+            A=A.split(",") #"\t"
+            list_length = len(A)
+
+            sample_key = A[0]
+               
+            if sample_key not in fdictionary.keys():
+                fdictionary[sample_key]=[]#[A[1]]
+                if list_length > 1:
+                    for item in A[1:list_length]:
+                        clean_item=item.strip('\n')
+                        if Duplicated:                        
+                            fdictionary[sample_key].append(clean_item)
+                        
+                        
+                        elif clean_item not in fdictionary[sample_key]: #won't write duplicate value
+                            fdictionary[sample_key].append(clean_item)
+            else:
+                if list_length > 1:
+                    for item in A[1:list_length]:
+                        clean_item=item.strip('\n')
+                        if Duplicated:
+                            fdictionary[sample_key].append(clean_item)
+                        elif clean_item not in fdictionary[sample_key]:
+                            fdictionary[sample_key].append(clean_item)
+
+
+      
+#print fdictionary
+
+
+table_dict_to_csv(outputfile, fdictionary)
